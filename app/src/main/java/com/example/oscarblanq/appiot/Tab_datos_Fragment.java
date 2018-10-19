5a53bc4630b945e7c87d913b72437b4ed1173ea4
Sentobi <viborro@epsg.upv.es>
2018-10-20 01:56:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/oscarblanq/appiot/Tab_datos_Fragment.java
@@ -0,0 +1,16 @@
+package com.example.oscarblanq.appiot;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab_datos_Fragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_bascula, container, false);
+    }
+}
